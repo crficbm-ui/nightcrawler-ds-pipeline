9de676309dec6c30c09114c58f82b0047bb3f34f
Thomas Schott <t.schott@quinten-france.com>
2024-09-03 11:20:33 +0100

    FunctionApp with durable functions

--- docker/function_app.py
+++ docker/function_app.py
@@ -1,14 +1,28 @@
 import azure.functions as func
+import azure.durable_functions as df
+
 import datetime
 import json
 import logging
 import nightcrawler.cli.main
 
-app = func.FunctionApp()
+app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="NcPipeline", auth_level=func.AuthLevel.FUNCTION)
-def NcPipeline(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="orchestrators/{functionName}")
+@app.durable_client_input(client_name="client")
+async def pipeline_start(req: func.HttpRequest, client):
+    function_name = req.route_params.get('functionName')
+    instance_id = await client.start_new(function_name)
+    response = client.create_check_status_response(req, instance_id)
+    return response
+
+# Orchestrator
+@app.orchestration_trigger(context_name="context")
+def hello_orchestrator(context):
     logging.info('Python HTTP trigger function processed a request.')
+    logging.info(f'context: {context}')
+
+    status = yield context.call_activity("hello", "In progress")
 
     keyword = req.params.get('keyword')
     country = req.params.get('country')
@@ -32,15 +46,15 @@ def NcPipeline(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
       nightcrawler.cli.main.run( [step, keyword, f'--country={country}'] )
+      status = yield context.call_activity("hello", "Finished")
     except Exception as e:
       import traceback
       logging.error(traceback.format_exc())
-      return func.HttpResponse(
-         e,
-         status_code=500
-      )
+      status = yield context.call_activity("hello", "Failed")
 
-    return func.HttpResponse(
-       "OK",
-       status_code=200
-    )
+    return [status]
+
+# Activity
+@app.activity_trigger(input_name="status")
+def hello(status: str):
+  return f'status: {status}'
