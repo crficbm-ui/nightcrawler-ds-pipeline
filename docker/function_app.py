2f31f3190c67da204e95c811062740f7091cea96
Thomas Schott <t.schott@quinten-france.com>
2024-09-03 12:52:03 +0100

    DEBUG

--- docker/function_app.py
+++ docker/function_app.py
@@ -6,55 +6,52 @@ import json
 import logging
 import nightcrawler.cli.main
 
-app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="orchestrators/{functionName}")
+@app.route(route="orchestrators/pipeline_orchestrator")
 @app.durable_client_input(client_name="client")
 async def pipeline_start(req: func.HttpRequest, client):
-    function_name = req.route_params.get('functionName')
-    instance_id = await client.start_new(function_name)
-    response = client.create_check_status_response(req, instance_id)
+    try:
+      logging.info(f'Trigger pipeline')
+      function_name = "pipeline_orchestrator"
+
+      req_data = { 
+        'keyword': req.params.get('keyword'),
+        'country': req.params.get('country','CH'),
+        'step':  req.params.get('step','fullrun')
+      }
+
+      instance_id = await client.start_new(function_name, None, req_data)
+      response = client.create_check_status_response(req, instance_id)
+    except Exception as e:
+      logging.error(e, exc_info=True) 
     return response
 
 # Orchestrator
 @app.orchestration_trigger(context_name="context")
-def hello_orchestrator(context):
-    logging.info('Python HTTP trigger function processed a request.')
-    logging.info(f'context: {context}')
+def pipeline_orchestrator(context):
+    logging.info('Pipeline started')
 
-    status = yield context.call_activity("hello", "In progress")
+    status = yield context.call_activity("pipeline_status", "Initialize")
 
-    keyword = req.params.get('keyword')
-    country = req.params.get('country')
-    step = req.params.get('step')
+    input_context = context.get_input()
+    keyword = input_context.get('keyword')
+    country = input_context.get('country','CH')
+    step = input_context.get('step','fullrun')
 
-    if not country:
-      country = 'CH'
-
-    if not step:
-      step = 'fullrun'
-
-    if not keyword:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            keyword = req_body.get('keyword')
-
-    logging.info(f'Keyword is {keyword} for country {country}')
+    logging.info(f'Running pipeline for keyword `{keyword}\' for country {country}')
+    status = yield context.call_activity("pipeline_status", "In progress")
 
     try:
       nightcrawler.cli.main.run( [step, keyword, f'--country={country}'] )
-      status = yield context.call_activity("hello", "Finished")
+      status = yield context.call_activity("pipeline_status", "Completed")
     except Exception as e:
-      import traceback
-      logging.error(traceback.format_exc())
-      status = yield context.call_activity("hello", "Failed")
+      logging.error(e, exc_info=True) 
+      status = yield context.call_activity("pipeline_status", f'Failed: {e}')
 
     return [status]
 
 # Activity
 @app.activity_trigger(input_name="status")
-def hello(status: str):
+def pipeline_status(status: str):
   return f'status: {status}'
