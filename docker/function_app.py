689e399d5303ae2257531861569c9665b99660f9
Mehdi Kadiri <m.kadiri@quinten-france.com>
2024-10-11 16:13:04 +0200

    Optionally use blob storage, manage crawl state, extract images and backoff retries

--- docker/function_app.py
+++ docker/function_app.py
@@ -4,7 +4,10 @@ import azure.durable_functions as df
 import json
 import logging
 import os
+import gc
 import nightcrawler.cli.main
+import nightcrawler.cli.full_pipeline
+from helpers.context import Context
 
 app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -15,12 +18,7 @@ app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
 @app.durable_client_input(client_name="client")
 async def pipeline_start(req: func.HttpRequest, client):
     try:
-        req_data = {
-            "keyword": req.params.get("keyword"),
-            "country": req.params.get("country", "CH"),
-            "step": req.params.get("step", "fullrun"),
-        }
-
+        req_data = {x: req.params.get(x) for x in req.params.keys()}
         instance_id = await client.start_new("pipeline_orchestrator", None, req_data)
         logging.info(f"Trigger pipeline with instance id {instance_id}")
         response = client.create_check_status_response(req, instance_id)
@@ -46,13 +44,8 @@ def sb_pipeline_start(msg: func.ServiceBusMessage):
 # Orchestrator
 @app.orchestration_trigger(context_name="context")
 def pipeline_orchestrator(context: df.DurableOrchestrationContext):
-    input_context = context.get_input()
-    req_data = {
-        "keyword": input_context.get("keyword"),
-        "country": input_context.get("country", "CH"),
-        "step": input_context.get("step", "fullrun"),
-    }
-
+    params = context.get_input()
+    req_data = {x: params.get(x) for x in params.keys()}
     status = yield context.call_activity("pipeline_work", req_data)
 
     return [status]
@@ -71,24 +64,22 @@ def pipeline_work(query: dict):
 
 
 def pipeline_wrapper(query):
-    keyword = query.get("keyword")
-    country = query.get("country", "CH")
-    step = query.get("step", "fullrun")
+    os.environ["NIGHTCRAWLER_USE_FILE_STORAGE"] = "false"
+    context = Context()
 
-    if keyword == "ALL":
-        os.environ["NIGHTCRAWLER_USE_FILE_STORAGE"] = "false"
-        os.environ["NIGHTCRAWLER_STORE_INTERMEDIATE"] = "false"
-        from libnightcrawler.context import Context
-        context = Context()
-        requests = context.get_crawl_requests()
-        if not requests:
-            logging.warning("No requests found in database")
-            return
-        for request in requests:
-            logging.info(f'Running pipeline for keyword `{request.keyword_value}\' for organization {request.organization.name}')
-            nightcrawler.cli.main.run(["fullrun", request.keyword_value, '--org', request.organization.name, "--keyword-id", str(request.keyword_id), "--case-id", str(request.case_id), "-n", "5"])
-    else:
-        logging.info(f'Running pipeline for keyword `{keyword}\' for country {country}')
-        nightcrawler.cli.main.run( [step, keyword, f'--country={country}'] )
+    requests = context.get_crawl_requests(case_id=int(query.get("case_id", 0)))
 
+    if not requests:
+        logging.warning("No requests found in database")
+        return
+    for request in requests:
+        logging.info(f'Running pipeline for keyword `{request.keyword_value}\' for organization {request.organization.name}')
+        # TODO: remove next line when fixed
+        request.number_of_results = 5
+        try:
+            nightcrawler.cli.full_pipeline.handle_request(context, request)
+        except Exception as e:
+            logging.critical(e, exc_info=True)
+            context.set_crawl_error(request.case_id, request.keyword_id, str(e))
+        gc.collect()
     logging.info('Pipeline terminated successfully')
