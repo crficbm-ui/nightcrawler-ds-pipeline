b52ee5e5f3c6a6b76ceb335284215778c70d937a
Mehdi Kadiri <m.kadiri@quinten-france.com>
2024-10-24 16:24:32 +0200

    Add doc string and type hints in function apps

--- docker/function_app.py
+++ docker/function_app.py
@@ -8,6 +8,7 @@ import gc
 import nightcrawler.cli.main
 import nightcrawler.cli.full_pipeline
 from helpers.context import Context
+from typing import Generator
 
 app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -16,7 +17,22 @@ app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
 ## HTTP
 @app.route(route="orchestrators/pipeline_orchestrator")
 @app.durable_client_input(client_name="client")
-async def pipeline_start(req: func.HttpRequest, client):
+async def pipeline_start(req: func.HttpRequest, client: df.DurableOrchestrationClient) -> func.HttpResponse:
+    """Triggers orchestrator from http call
+
+        Parameters
+        ----------
+        req: func.HttpRequest
+            Http request
+        client: df.DurableOrchestrationClient
+            Client for starting, querying, terminating and raising events to orchestration instances.
+
+        Returns
+        -------
+        func.HttpResponse
+            HttpResponse that contains useful information for checking the status of the specified instance
+    """
+
     try:
         req_data = {x: req.params.get(x) for x in req.params.keys()}
         instance_id = await client.start_new("pipeline_orchestrator", None, req_data)
@@ -32,7 +48,14 @@ async def pipeline_start(req: func.HttpRequest, client):
 @app.service_bus_queue_trigger(
     arg_name="msg", queue_name="run-keyword", connection="ServiceBus"
 )
-def sb_pipeline_start(msg: func.ServiceBusMessage):
+def sb_pipeline_start(msg: func.ServiceBusMessage) -> None:
+    """Triggers pipeline from message in Service Bus
+
+        Parameters
+        ----------
+        msg: func.ServiceBusMessage
+            Message from Service Bus
+    """
     logging.info("Python ServiceBus queue trigger processed message")
     try:
         req_data = json.loads(msg.get_body().decode("utf-8"))
@@ -43,7 +66,19 @@ def sb_pipeline_start(msg: func.ServiceBusMessage):
 
 # Orchestrator
 @app.orchestration_trigger(context_name="context")
-def pipeline_orchestrator(context: df.DurableOrchestrationContext):
+def pipeline_orchestrator(context: df.DurableOrchestrationContext) -> Generator[str, str, list[str]]:
+    """Orchestrate a pipeline
+
+        Parameters
+        ----------
+        context: df.DurableOrchestrationContext
+            Azure context for durable function
+
+        Returns
+        -------
+        list[str]
+            List of results for each activity
+    """
     params = context.get_input()
     req_data = {x: params.get(x) for x in params.keys()}
     status = yield context.call_activity("pipeline_work", req_data)
@@ -53,7 +88,19 @@ def pipeline_orchestrator(context: df.DurableOrchestrationContext):
 
 # Activity
 @app.activity_trigger(input_name="query")
-def pipeline_work(query: dict):
+def pipeline_work(query: dict) -> str:
+    """Run single activity
+
+        Parameters
+        ----------
+        query: dict
+            Query parameters
+
+        Returns
+        -------
+        str
+            Success or Failure error
+    """
     try:
         pipeline_wrapper(query)
     except Exception as e:
@@ -63,7 +110,15 @@ def pipeline_work(query: dict):
     return "Success"
 
 
-def pipeline_wrapper(query):
+def pipeline_wrapper(query: dict) -> None:
+    """Run full pipeline on a single case or all cases
+
+        Parameters
+        ----------
+        query: dict
+            Query parameters
+
+    """
     os.environ["NIGHTCRAWLER_USE_FILE_STORAGE"] = "false"
     context = Context()
 
