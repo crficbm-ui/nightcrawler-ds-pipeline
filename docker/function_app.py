5a2fe3b4dbbba9b5d150b310f68755b6f1bcbd97
Mehdi Kadiri <m.kadiri@quinten-france.com>
2024-11-29 17:54:43 +0100

    Add scheduled crawls

--- docker/function_app.py
+++ docker/function_app.py
@@ -11,9 +11,33 @@ from nightcrawler.context import Context
 from typing import Generator
 
 app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
-
+os.environ["NIGHTCRAWLER_USE_FILE_STORAGE"] = "false"
+os.environ["NIGHTCRAWLER_POSTGRES_AUTO_MIGRATE"] = "false"
 
 # Triggers
+
+## Scheduled: every day at 10 p.m.
+@app.function_name(name="dailyUpdates")
+@app.timer_trigger(schedule="0 0 22 * * *",
+              arg_name="timer",
+              run_on_startup=False)
+@app.durable_client_input(client_name="client")
+async def daily_update(timer: func.TimerRequest, client: df.DurableOrchestrationClient):
+    """Triggers orchestrator from scheduled call
+
+    Parameters
+    ----------
+    timer: func.TimerRequest
+        Request parameters
+    client: df.DurableOrchestrationClient
+        Client for starting, querying, terminating and raising events to orchestration instances.
+    """
+
+    logging.warning('Daily updates triggered')
+    instance_id = await client.start_new("pipeline_orchestrator", None, dict())
+    logging.warning('Created instance %s', instance_id)
+
+
 ## HTTP
 @app.route(route="orchestrators/pipeline_orchestrator")
 @app.durable_client_input(client_name="client")
@@ -37,6 +61,8 @@ async def pipeline_start(
 
     try:
         req_data = {x: req.params.get(x) for x in req.params.keys()}
+        req_data.pop("code", None)
+        logging.info(f"HTTP Trigger pipeline with params {req_data}")
         instance_id = await client.start_new("pipeline_orchestrator", None, req_data)
         logging.info(f"Trigger pipeline with instance id {instance_id}")
         response = client.create_check_status_response(req, instance_id)
@@ -85,10 +111,20 @@ def pipeline_orchestrator(
     """
     params = context.get_input()
     req_data = {x: params.get(x) for x in params.keys()}
-    status = yield context.call_activity("pipeline_work", req_data)
+    logging.warning('Pipeline orchestration triggered with data: %s', req_data)
+    if req_data:
+        status = yield context.call_activity("pipeline_work", req_data)
+        return [status]
 
-    return [status]
-
+    # no params == scheduled triggers
+    nc_context = Context()
+    # Disable expired cases
+    nc_context.disable_expired_cases()
+    # Call one activity for each active keyword
+    keywords = nc_context.get_today_keywords()
+    tasks = [context.call_activity("pipeline_work", {"keyword_id": x}) for x in keywords]
+    results = yield context.task_all(tasks)
+    return results
 
 # Activity
 @app.activity_trigger(input_name="query")
@@ -123,22 +159,20 @@ def pipeline_wrapper(query: dict) -> None:
         Query parameters
 
     """
-    os.environ["NIGHTCRAWLER_USE_FILE_STORAGE"] = "false"
-    context = Context()
-
-    requests = context.get_crawl_requests(case_id=int(query.get("case_id", 0)))
+    nc_context = Context()
+    requests = nc_context.get_crawl_requests(**query)
 
     if not requests:
-        logging.warning("No requests found in database")
+        logging.warning("No requests found in database for query %s", query)
         return
     for request in requests:
         logging.info(
             f"Running pipeline for keyword `{request.keyword_value}' for organization {request.organization.name}"
         )
         try:
-            nightcrawler.cli.full_pipeline.handle_request(context, request)
+            nightcrawler.cli.full_pipeline.handle_request(nc_context, request)
         except Exception as e:
             logging.critical(e, exc_info=True)
-            context.set_crawl_error(request.case_id, request.keyword_id, str(e))
+            nc_context.set_crawl_error(request.case_id, request.keyword_id, str(e))
         gc.collect()
     logging.info("Pipeline terminated successfully")
